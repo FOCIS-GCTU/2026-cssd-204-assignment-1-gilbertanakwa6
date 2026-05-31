995a48709b6cb4aa5fa4890b905cf037c1f3760c
gilbertanakwa6 <gilbertanakwa6@gmail.com>
2026-05-31 13:46:36 -0500

    Add files via upload

---
+++ GNA.py.py
@@ -0,0 +1,30 @@
+# File: Initials.py
+# Description: Prints out the initials of my name (Gilbert Nana Anakwa)
+# Assignment Number: 1
+#
+# Name: GILBERT NANA ANAKWA
+# STUDENT ID: 2425405055
+# Email: 2425405055@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+#
+# On my honor, Gilbert Nana Anakwa, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+def main():
+    # the codes below will print my initials (G.N.A)in stylish large letters
+    print()
+    print("...GNA")
+    print()
+
+    print("...GGGGGGGGGGGG........NNNN      NNNN........AAAAAAAAAA......")
+    print("...GGGGGGGGGGGG........NNNNN     NNNN........AAAAAAAAAA......")
+    print("...GGGG................NNNNNN    NNNN........AA......AA......")
+    print("...GGGG................NNNN NNN  NNNN........AA......AA......")
+    print("...GGGG...GGGGG........NNNN..NNN.NNNN........AAAAAAAAAA......")
+    print("...GGGG...GGGGG........NNNN...NNNNNNN........AAAAAAAAAA......")
+    print("...GGGG......GG........NNNN....NNNNNN........AA......AA......")
+    print("...GGGGGGGGGGGG........NNNN.....NNNNN........AA......AA......")
+    print("...GGGGGGGGGGGG...**...NNNN......NNNN...**...AA......AA....**")
+    print("...GGGGGGGGGGGG...**...NNNN......NNNN...**...AA......AA....**")
+
+main()
